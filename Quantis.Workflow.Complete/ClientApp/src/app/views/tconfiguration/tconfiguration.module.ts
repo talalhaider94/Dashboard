38f1bca1e59086890afc098378452b1a0698f324
Talal Haider <talalhaider93@gmail.com>
2019-06-10 10:39:16 +0500

    Update

--- Quantis.Workflow.Complete/ClientApp/src/app/views/tconfiguration/tconfiguration.module.ts
+++ Quantis.Workflow.Complete/ClientApp/src/app/views/tconfiguration/tconfiguration.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ChartsModule } from 'ng2-charts/ng2-charts';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { CommonModule } from '@angular/common';
 
 import { TConfigurationComponent } from './tconfiguration.component';
 import { TConfigurationRoutingModule } from './tconfiguration-routing.module';
 import {DataTablesModule} from 'angular-datatables';
 
+
 @NgModule({
+  declarations: [TConfigurationComponent],
   imports: [
-    FormsModule,
+    CommonModule,
     TConfigurationRoutingModule,
-    ChartsModule,
-    BsDropdownModule,
-    ButtonsModule.forRoot(),
     DataTablesModule
-  ],
-  declarations: [ TConfigurationComponent ]
+  ]
 })
 export class TConfigurationModule { }
